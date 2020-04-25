e5d2732c13db744ccd55fffd67cb112c9c4a1da0
harriethorobinworley <harriet.h@thoughtworks.com>
2020-04-25 16:17:46 +0100

    checking if event is null before calling on Pronoun

--- Assets/Scripts/Pronoun.cs
+++ Assets/Scripts/Pronoun.cs
@@ -3,29 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Pronoun : MonoBehaviour
-{
+public class Pronoun : MonoBehaviour {
 
-    public delegate void OnPronounCaught();
+    public delegate void OnPronounCaught ();
     public static OnPronounCaught onPronounCaught;
 
     private string correctPronoun = "they";
 
     private bool clicked = false;
 
-    public void OnMouseDown()
-    {
-        if (!clicked)
-        {
-            PronounCaught();
+    public void OnMouseDown () {
+        if (!clicked) {
+            PronounCaught ();
             clicked = true;
         }
     }
 
-    private void PronounCaught()
-    {
-        onPronounCaught();
-        gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = correctPronoun;
-        gameObject.GetComponentInChildren<ParticleSystem>().Play();
+    private void PronounCaught () {
+        if (onPronounCaught != null) {
+            onPronounCaught ();
+        }
+        gameObject.GetComponent<TMPro.TextMeshProUGUI> ().text = correctPronoun;
+        gameObject.GetComponentInChildren<ParticleSystem> ().Play ();
     }
 }
