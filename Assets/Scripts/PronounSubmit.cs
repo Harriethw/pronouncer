7531f7af68645008ab142c30e53e55fd17a7d9d0
harriethorobinworley <harriet.h@thoughtworks.com>
2020-05-02 14:34:23 +0100

    first pass at setting chosen pronouns in a scene

---
+++ Assets/Scripts/PronounSubmit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PronounSubmit : MonoBehaviour {
+
+    public TMP_InputField tmpInputField;
+
+    public void SetPronoun1 () {
+        PronounValues.SetPronoun1 (tmpInputField.text);
+    }
+
+    public void SetPronoun2 () {
+        PronounValues.SetPronoun2 (tmpInputField.text);
+    }
+
+    public void OnSubmit () {
+        Debug.Log ("submitting " + PronounValues.GetPronoun1 () + PronounValues.GetPronoun2 ());
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/PronounValues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PronounValues {
+    private static string chosenPronoun1;
+    private static string chosenPronoun2;
+
+    public static void SetPronouns (string pronoun1, string pronoun2) {
+        chosenPronoun1 = pronoun1;
+        chosenPronoun2 = pronoun2;
+        Debug.Log ("chosen pronouns are " + chosenPronoun1 + " " + chosenPronoun2);
+    }
+
+    public static void SetPronoun1 (string pronoun1) {
+        chosenPronoun1 = pronoun1;
+        Debug.Log ("chosen pronoun1 is " + chosenPronoun1);
+    }
+
+    public static void SetPronoun2 (string pronoun2) {
+        chosenPronoun2 = pronoun2;
+        Debug.Log ("chosen pronoun2 is " + chosenPronoun2);
+    }
+
+    public static string GetPronoun1 () {
+        Debug.Log ("returning " + chosenPronoun1);
+        return chosenPronoun1;
+    }
+
+    public static string GetPronoun2 () {
+        Debug.Log ("returning " + chosenPronoun2);
+        return chosenPronoun2;
+    }
+}
