77211db5fcc2f46c8493d7530d96b933ea0b43c0
harriethorobinworley <harriethw@gmail.com>
2020-05-10 16:28:22 +0100

    start to programmatically generate speech bubbles

---
+++ Assets/Scripts/SpeechController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechController : MonoBehaviour {
+
+    public GameObject speechBubble;
+    // Start is called before the first frame update
+    void Start () {
+        GenerateSpeech ();
+    }
+
+    private void GenerateSpeech () {
+        for (int i = 0; i < 10; i++) {
+            GameObject speech = Instantiate (speechBubble);
+            speech.transform.SetParent (gameObject.transform, false);
+            Debug.Log (speech.transform.GetSiblingIndex ());
+        }
+    }
+}
