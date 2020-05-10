94c06509324b8f39cbc07c2a5b8fb62943f7c7d2
harriethorobinworley <harriethw@gmail.com>
2020-05-10 15:32:39 +0100

    making encouragement words independent

--- Assets/Scripts/Encouragement.cs
+++ Assets/Scripts/Encouragement.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Encouragement : MonoBehaviour
-{
+public class Encouragement : MonoBehaviour {
+
+    private List<string> encouragementPhrases = new List<string> { "Nice work!", "Awesome!", "Well done!", "You got this!" };
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+        Pronoun.onPronounCaught += this.ShowEncouragement;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void ShowEncouragement () {
+        gameObject.GetComponentInChildren<TMPro.TextMeshProUGUI> ().text = encouragementPhrases[Random.Range (0, encouragementPhrases.Count)];
+        gameObject.GetComponent<Animator> ().Play ("fade");
     }
 }
