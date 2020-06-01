501f59a4d4a3b43511b37f97da5e7fbca2546d0a
harriethorobinworley <harriethw@gmail.com>
2020-06-01 09:08:22 +0100

    dynamically adjust height of speech bubbles based on text

--- Assets/Scripts/SpeechController.cs
+++ Assets/Scripts/SpeechController.cs
@@ -19,11 +19,35 @@ public class SpeechController : MonoBehaviour
 
     private void GenerateSpeech()
     {
-        for (int i = 0; i < 10; i++)
+        List<string> formattedChatter = GetChatter();
+        for (int i = 0; i < formattedChatter.Count; i++)
         {
             GameObject speech = Instantiate(speechBubble);
             speech.transform.SetParent(gameObject.transform, false);
+            speech.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = formattedChatter[i];
+            SetHeight(speech.transform);
         }
+    }
+
+    private List<string> GetChatter()
+    {
+        List<string> formattedChatter = new List<string>();
+        foreach (var text in SpeechCopy.chatter)
+        {
+            string newText = "";
+            newText = text.Replace("(wrongPronoun1)", PronounValues.GetWrongPronouns1()[0]);
+            newText = newText.Replace("(wrongPronoun2)", PronounValues.GetWrongPronouns2()[0]);
+            formattedChatter.Add(newText);
+        }
+        return formattedChatter;
+    }
+
+    private void SetHeight(Transform speechTransform)
+    {
+        Canvas.ForceUpdateCanvases();
+        float textHeight = speechTransform.GetChild(0).GetComponent<RectTransform>().rect.height;
+        float currentWidth = speechTransform.GetComponent<RectTransform>().rect.width;
+        speechTransform.GetComponent<RectTransform>().sizeDelta = new Vector2(currentWidth, textHeight);
     }
 
     public void Interrupt()
---
+++ Assets/Scripts/SpeechCopy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpeechCopy {
+
+    public static string[] chatter = new string[] {
+        "hey how did that big meeting go?",
+        "Oh yeah it was pretty good, we talked over the KPI's for this sprint.",
+        "Awesome.",
+        "(name) gave (wrongPronoun2) presentation about donuts and it was great",
+        "oh cool! I've heard about (name)'s and (wrongPronoun2) donut theory!",
+        "Yeah actually (wrongPronoun1) had a good idea about deep fat frying that I think could help our cycle time",
+        "Let's table that idea for now, I don't wanna overload (wrongPronoun2) plate!"
+    };
+
+    public static string interruption = "Hey actually my pronouns are (rightPronoun1)/(rightPronoun2)";
+
+    public static string[] apologies = new string[] {
+        "Oh I'm sorry! I'll do better next time",
+        "Yeah sorry about that"
+    };
+
+}
